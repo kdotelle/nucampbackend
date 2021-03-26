3407f25c196da1bef87c7b7629394787f9c74d40
Kristen Lloyd <lloyd.kristen91@gmail.com>
2021-03-25 21:09:33 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "900799697406287",
+    clientSecret: "caa03c9ea0b81aa3a690dcf171de3521",
+  },
 };
